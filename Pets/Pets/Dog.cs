8985283dcb73f8d60e6b44540ee0fd593fe157e6
7staff <42589270+7staff@users.noreply.github.com>
2019-03-07 22:14:39 -0600

    Add files via upload

---
+++ Pets/Pets/Dog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pets
+{
+    public class Dog : Pet
+    {
+        public string type2;
+        public string name2;
+        public string owner2;
+        public double weight2;
+        public Dog(string name, string owner, double weight) : base(string.Empty, name, owner, weight)
+        {
+            
+            name = name2;
+            owner = owner2;
+            weight = weight2;
+        }
+        public string bark(int count)
+        {
+            string bark = "";
+            for (int i = 0; i < count; i++)
+            {
+                bark = bark + "bark!";
+            }
+            return bark;
+        }
+    }
+}
---
+++ Pets/Pets/Pet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pets
+{
+    public class Pet
+    {
+        public string type;
+        public string name;
+        public string owner;
+        public double weight;
+        public Pet(string type2, string name2, string owner2, double weight2)
+        {
+            type = type2;
+            name = name2;
+            owner = owner2;
+            weight = weight2;
+        }
+        public string getTag()
+        {
+            return "If lost, call " + owner;
+        }
+    }
+}
